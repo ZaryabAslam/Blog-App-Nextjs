d97e9dc12f77fb650be4f50ada09c5a4ce3d198f
Zaryab Aslam <zaryab.aslam@systemsltd.com>
2023-03-27 18:18:41 +0500

    removing extra console

--- components/posts/posts-grid.js
+++ components/posts/posts-grid.js
@@ -4,8 +4,6 @@ import classes from './posts-grid.module.css';
 function PostsGrid(props){
     //props get from featuredposts and passing to single item
 const {posts} = props;
-console.log(props, 'props');
-console.log(posts, 'posts');
 return (
     <ul className={classes.grid}>
         {posts.map(post => <PostItem key={post.slug} post={post} />)}
