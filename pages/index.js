2d393cf095b49684e0dd6564d3bd60e300ed79e3
Zaryab Aslam <zaryab.aslam@systemsltd.com>
2023-03-28 17:46:50 +0500

    adding getstaticprops in home and all posts page, and using data from markdown as props means data soruce to show data, omitting dummydtaa variable

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,71 @@
 import { Fragment } from "react";
 import Hero from '../components/home-page/hero';
 import FeaturedPosts from '../components/home-page/featured-posts';
+import { getFeaturedPosts } from "../lib/post-util";
 
 // home page where heros section and featured posts will be shown
 //dummy data for posts to be passed to featured posts
-const DUMMY_POSTS = [
-    {
-        slug: 'getting-started-with-nextjs',
-        title: 'Getting Started with NextJS',
-        image: 'getting-started-nextjs.png',
-        excerpt:
-          'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-        date: '2022-02-10',
-      },
-      {
-        slug: 'getting-started-with-nextjs2',
-        title: 'Getting Started with NextJS',
-        image: 'getting-started-nextjs.png',
-        excerpt:
-          'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-        date: '2022-02-10',
-      },
-      {
-        slug: 'getting-started-with-nextjs3',
-        title: 'Getting Started with NextJS',
-        image: 'getting-started-nextjs.png',
-        excerpt:
-          'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-        date: '2022-02-10',
-      },
-      {
-        slug: 'getting-started-with-nextjs4',
-        title: 'Getting Started with NextJS',
-        image: 'getting-started-nextjs.png',
-        excerpt:
-          'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-        date: '2022-02-10',
-      },
-    ];
-function HomePage(){
+// const DUMMY_POSTS = [
+//     {
+//         slug: 'getting-started-with-nextjs',
+//         title: 'Getting Started with NextJS',
+//         image: 'getting-started-nextjs.png',
+//         excerpt:
+//           'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+//         date: '2022-02-10',
+//       },
+//       {
+//         slug: 'getting-started-with-nextjs2',
+//         title: 'Getting Started with NextJS',
+//         image: 'getting-started-nextjs.png',
+//         excerpt:
+//           'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+//         date: '2022-02-10',
+//       },
+//       {
+//         slug: 'getting-started-with-nextjs3',
+//         title: 'Getting Started with NextJS',
+//         image: 'getting-started-nextjs.png',
+//         excerpt:
+//           'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+//         date: '2022-02-10',
+//       },
+//       {
+//         slug: 'getting-started-with-nextjs4',
+//         title: 'Getting Started with NextJS',
+//         image: 'getting-started-nextjs.png',
+//         excerpt:
+//           'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+//         date: '2022-02-10',
+//       },
+//     ];
+
+//now we are not using dummyposts but we are using data stored in markdown file
+function HomePage(props){
     return (
         <Fragment>
             <Hero />
-            <FeaturedPosts posts={DUMMY_POSTS} />
+            {/* we used dummyposts earlier now we will use props get from static props */}
+            {/* <FeaturedPosts posts={DUMMY_POSTS} /> */}
+            <FeaturedPosts posts={props.posts} />
         </Fragment>
     )
+}
+
+//using getstaticprps, bcz we need this data at buildtime.
+// also the data dont change much so it is good to use staticprops
+export  function getStaticProps() {
+
+  const featuredPosts = getFeaturedPosts();
+  return {
+    props: {
+      posts: featuredPosts,
+      
+    }, // will be passed to the page component as props
+    // ye can use revalidate which takes value in secondoncds to send request againt. example revalidate after 10 mints. so data will be refreshed after 10 mints after deployement also
+    // revalidate chalta hy after some time jb data change ho aur ap uspage py wapasa ao after some time data updated milega khud
+    //revalidate: 10,
+  }
 }
 
 export default HomePage;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,45 +1,61 @@
 import { Fragment } from "react";
 import AllPosts from '../../components/posts/all-posts';
+import { getAllPosts } from "../../lib/post-util";
 
-const DUMMY_POSTS = [
-  {
-      slug: 'getting-started-with-nextjs',
-      title: 'Getting Started with NextJS',
-      image: 'getting-started-nextjs.png',
-      excerpt:
-        'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-      date: '2022-02-10',
-    },
-    {
-      slug: 'getting-started-with-nextjs2',
-      title: 'Getting Started with NextJS',
-      image: 'getting-started-nextjs.png',
-      excerpt:
-        'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-      date: '2022-02-10',
-    },
-    {
-      slug: 'getting-started-with-nextjs3',
-      title: 'Getting Started with NextJS',
-      image: 'getting-started-nextjs.png',
-      excerpt:
-        'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-      date: '2022-02-10',
-    },
-    {
-      slug: 'getting-started-with-nextjs4',
-      title: 'Getting Started with NextJS',
-      image: 'getting-started-nextjs.png',
-      excerpt:
-        'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-      date: '2022-02-10',
-    },
-  ];
+// const DUMMY_POSTS = [
+//   {
+//       slug: 'getting-started-with-nextjs',
+//       title: 'Getting Started with NextJS',
+//       image: 'getting-started-nextjs.png',
+//       excerpt:
+//         'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+//       date: '2022-02-10',
+//     },
+//     {
+//       slug: 'getting-started-with-nextjs2',
+//       title: 'Getting Started with NextJS',
+//       image: 'getting-started-nextjs.png',
+//       excerpt:
+//         'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+//       date: '2022-02-10',
+//     },
+//     {
+//       slug: 'getting-started-with-nextjs3',
+//       title: 'Getting Started with NextJS',
+//       image: 'getting-started-nextjs.png',
+//       excerpt:
+//         'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+//       date: '2022-02-10',
+//     },
+//     {
+//       slug: 'getting-started-with-nextjs4',
+//       title: 'Getting Started with NextJS',
+//       image: 'getting-started-nextjs.png',
+//       excerpt:
+//         'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+//       date: '2022-02-10',
+//     },
+//   ];
 
 // this is allposts page
-function AllPostsPage() {
+function AllPostsPage(props) {
   // this can be get in allposts as props.posts bcz posts is passed here
-  return <AllPosts posts={DUMMY_POSTS} />
+  //dummy posts now will not be used as we use staticprops now
+  // return <AllPosts posts={DUMMY_POSTS} />
+  return <AllPosts posts={props.posts} />
 }
 
+
+export  function getStaticProps() {
+
+  const allPosts = getAllPosts();
+  return {
+    props: {
+      posts: allPosts
+      
+    }, // will be passed to the page component as props
+    // ye can use revalidate which takes value in secondoncds to send request againt. example revalidate after 10 mints. so data will be refreshed after 10 mints after deployement also
+    //revalidate: 10,
+  }
+}
 export default AllPostsPage;
