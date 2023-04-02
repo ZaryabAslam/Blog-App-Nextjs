5e215835f265d30924fb89791cc7bccf70ed5dfa
Zaryab Aslam <zaryab.aslam@systemsltd.com>
2023-04-02 22:54:17 +0500

    adding slug page with staticpath props

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -4,25 +4,26 @@ import ReactMarkdown from 'react-markdown';
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
 
-const DUMMY_POST = {
-  slug: 'getting-started-with-nextjs',
-  title: 'Getting Started with NextJS',
-  image: 'getting-started-nextjs.png',
-  date: '2022-02-10',
-  content: '# This is a first post',
-};
+// const DUMMY_POST = {
+//   slug: 'getting-started-with-nextjs',
+//   title: 'Getting Started with NextJS',
+//   image: 'getting-started-nextjs.png',
+//   date: '2022-02-10',
+//   content: '# This is a first post',
+// };
 
 //in conttent it is markdown and refered as title (anythung written with hash sign)
 //this page is pagecontent for a single post(which pdp u can say in dynamics world)
-function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+function PostContent(props) {
+  const { post } = props;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   return (
     <article className={classes.content}>
         {/* this is shown as top of pdp below navbar. it contains image and title of post */}
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      <PostHeader title={post.title} image={imagePath} />
       {/* // now .content which is rapped in markdown will be served as H1 , bcz in markdown # IS H1. we using it to reduce code */}
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,10 +1,48 @@
 import PostContent from '../../components/posts/post-detail/post-content';
+import { getPostsFiles, getPostData } from '../../lib/post-util';
 
 
 // this is particular post page
-function PostDetailPage() {
+function PostDetailPage(props) {
     //this post content is content for a single post on pdp page
-    return <PostContent />
+    return <PostContent post={props.post} />;
+  }
+
+  export function getStaticProps(context) {
+    // params is cont we get from context
+    const { params } = context;
+    // slug is filename here we get from context. whatever dynamic filename it is. jo url main hoga wo milega
+    const { slug } = params;
+  
+    // post data milega
+    const postData = getPostData(slug);
+  
+    return {
+      props: {
+        post: postData,
+      },
+      // after deployment agar file main change aega to khud update hoga
+      revalidate: 600,
+    };
+  }
+  
+  //static path use hoga static props k sath. usko btany k lye k paths konsy use hony bhai. baqi staticprops wesy he use hoga for props
+  export function getStaticPaths() {
+    //ye sb filenames lega
+    const postFilenames = getPostsFiles();
+    // ye filenames main sy extension hata dega q k path main filenames dety hain extension nae
+
+    const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ''));
+  
+    return {
+      //paths ko dena parta hy aesa he object jismy params ho aur us params k andar slug ko file name hy aur sndar wala slug uski value
+      paths: slugs.map((slug) => ({ params: { slug: slug } })),
+      //fallback false mtlb baqi sary paths py 404 aega jo in paths k siwa ho humny uper btae hue hain jo file sy araae hain
+      //fllback truem -> jb visit karengy tb data bnaega aur koe fllaback msg b bnao ka abhi tk data load na hua to user ko kya dikhana like loading etc. ismy paths empty rakho 
+      //fallback vlocing -> wait untill page has completed ;oading then show user data -> blocking tb use kr jb 1000 blogs hain koe koe to bht kam khulty hain islye sary prerender nae karny chahye
+      //fallback false mtlb koe aur page 404 dyga. ye waha sahe hy jaha page kam ho aur prerendering karny main itny zyada nae to time kam lagege slow nae hoga. imsy paths deny parengy sary
+      fallback: false,
+    };
   }
 
 export default PostDetailPage;
