b75f408ba5c9702892f1259329b65792daeac294
Zaryab Aslam <zaryab.aslam@systemsltd.com>
2023-04-03 17:55:08 +0500

    added contact api without db, simple api endpoint likha hy jismy body nikal k just console karae hy

---
+++ pages/api/contact.js
@@ -0,0 +1,38 @@
+
+//this is api route and will be api/contact
+
+function handler(req, res) {
+    //if request is post we do this else nothing right now
+    if (req.method === 'POST') {
+        // body parsed he milti hy next so just take out values jo ap body main send kar rhay hain while making request
+      const { email, name, message } = req.body;
+        
+      //backend py b validation chedk karlo koe input b faulty ho body main to status dedengy 422 k fail hy valid nae hy input
+      if (
+        !email ||
+        !email.includes('@') ||
+        !name ||
+        name.trim() === '' ||
+        !message ||
+        message.trim() === ''
+      ) {
+        res.status(422).json({ message: 'Invalid input.' });
+        return;
+      }
+  
+      // Store it in a database
+      const newMessage = {
+        email,
+        name,
+        message,
+      };
+  
+      console.log(newMessage);
+  
+      res
+        .status(201)
+        .json({ message: 'Successfully stored message!', message: newMessage });
+    }
+  }
+  
+  export default handler;
